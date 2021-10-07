b1fc01b6eddc844911596a8c918fb2f4fe3471c1
Lukyn319 <91658013+Lukyn319@users.noreply.github.com>
2021-10-07 11:39:45 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-povoleno = BeatFraction.WHOLE
+povoleno = False
 
-def on_burron_pressed_a():
+def on_button_pressed_a():
     global povoleno
     povoleno= True
-input.on_button_pressed(Button.A, on_Button_pressed_a)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 
 def on_forever():
     if povoleno:
-        basic.show_icon(iconNames.YES)
+        basic.show_icon(IconNames.YES)
+    else:
+        basic.show_icon(IconNames.NO)
+basic.forever(on_forever)
